c02bf51746028956e65efecf15b248068c865a43
Alud24k <ifem.perez@gmail.com>
2023-12-18 20:28:07 -0300

    Se agrego mensajes por consola, documento HTML y ventanas emergentes

---
+++ js/01-variables.js
@@ -0,0 +1,13 @@
+// Comentario de una sola linea
+
+/* Comentario de varias lineas */
+
+// Mostrar mensaje en la consola
+console.log("Mensaje por consola");
+
+// Mostrar un mensaje en el documento de HTML
+document.write('<p class="text-center text-primary">Primer mensaje en JS</p>');
+
+// Ventanas emergentes
+alert("Ejemplo de una alerta");
+
