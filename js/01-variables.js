55e96b03480414f42cd6fc573f83b3eda2f6bf79
Alud24k <ifem.perez@gmail.com>
2023-12-18 21:18:11 -0300

    Agregamos variables creadas con let y const

--- js/01-variables.js
+++ js/01-variables.js
@@ -11,3 +11,18 @@ document.write('<p class="text-center text-primary">Primer mensaje en JS</p>');
 // Ventanas emergentes
 alert("Ejemplo de una alerta");
 
+// Declarar e inicializar una variable
+// Declarar o crear una variable (var - let -const)
+
+let regalo = "iPhone 15 Pro Max";
+
+document.write("Regalo de navidad 2023: " + regalo + "<br>");
+
+// Modificar el contenido de una variable creada con la variable "let"
+regalo = "RTX 4090 ROG STRIX";
+
+document.write("Regalo de navidad 2023: " + regalo);
+
+const urlUniverse = "https://universe.rollingcodeschool.com";
+
+document.write("<br>Pagina de Universe: " + urlUniverse);
