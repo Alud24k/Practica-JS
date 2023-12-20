66be5f699d7127edd5570249c9e96ba41625e0a3
Alud24k <ifem.perez@gmail.com>
2023-12-20 19:53:43 -0300

    Agregamos un ejemplo de promt para ingresar el valor de grados celsius

--- js/01-variables.js
+++ js/01-variables.js
@@ -9,7 +9,7 @@ console.log("Mensaje por consola");
 document.write('<p class="text-center text-primary">Primer mensaje en JS</p>');
 
 // Ventanas emergentes
-alert("Ejemplo de una alerta");
+// alert("Ejemplo de una alerta");
 
 // Declarar e inicializar una variable
 // Declarar o crear una variable (var - let -const)
@@ -30,4 +30,21 @@ document.write("<br>Pagina de Universe: " + urlUniverse);
 let precio = 1100;
 let impuesto = 600;
 
-document.write("<br>El precio del iPhone 15 es: $" + (precio + impuesto) + ' USD');
+document.write(
+  "<br>El precio del iPhone 15 es: $" + (precio + impuesto) + " USD"
+);
+
+// parseInt('50')
+// parseFloat('50.3')
+let gradosCelsius = parseInt(prompt("Ingrese el valor en grados centigrados"));
+let gradosFahrenheit = (gradosCelsius * 9) / 5 + 32;
+
+document.write(
+  "<br>" +
+    gradosCelsius +
+    "°C grados celsius son equivalentes a " +
+    gradosFahrenheit +
+    "°F grados farenheit"
+);
+
+document.write("<br>Otro ejemplo: " + (gradosCelsius + 32));
